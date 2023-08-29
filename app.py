63b0951ea171de41025c9a18deb8882a39735f97
Jabbar Sam <sam.jari@live.se>
2023-08-29 16:05:46 +0200

    firebase
    working with cart not working get

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import json
-from flask import Flask, render_template, request, session, jsonify, redirect, send_from_directory, url_for
+from flask import Flask, render_template, request, session, jsonify, redirect, send_from_directory
 import os
-import sqlite3
-from shutil import copyfile
 import logging
-import firebase_admin  # Import firebase_admin module
-from firebase_admin import credentials, firestore  # Import specific submodules
+import firebase_admin
+from firebase_admin import credentials, db, firestore
 
 app = Flask(__name__, static_folder='./static')
 app.secret_key = 'your_secret_key_here'
 
-# Initialize Firebase with your project's credentials
 cred = credentials.Certificate("credentials.json")
-firebase_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+firebase_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://sns-jewllery-default-rtdb.europe-west1.firebasedatabase.app'
+})
+db_ref = db.reference()
+
+firestore_db = firestore.client()
+
+@app.route('/')
+def index():
+    try:
+        products = get_webshop_from_firestore()
+        return render_template('index.html', products=products)
+    except Exception as e:
+        logging.error(str(e))
+        return render_template('index.html', products=[])
 
 # Konfigurera sökvägen till "orders" mappen
 app.config['ORDERS_FOLDER'] = os.path.join(app.static_folder, 'orders')
@@ -29,30 +39,22 @@ def process_image_and_save(name, processed_image):
 def get_image(filename):
     return send_from_directory('static/orders', filename)
 
-# Databasanslutning
-def get_items_db_connection():
-    conn = sqlite3.connect('items.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_webshop_db_connection():
-    # Establish a connection to the database
-    conn = sqlite3.connect('webshop.db')
-    cursor = conn.cursor()
-
-    # Fetch product details from the database
-    cursor.execute('SELECT name, price FROM products')
-    products = cursor.fetchall()
-
-    conn.close()
+def get_webshop_from_firestore():
+    webshop_ref = firestore_db.collection('webshop')
+    webshop_docs = webshop_ref.stream()
+    products = []
+    for doc in webshop_docs:
+        product_data = doc.to_dict()
+        products.append({
+            'name': product_data.get('name', ''),
+            'price': product_data.get('price', 0),
+            'weight': product_data.get('weight', 0),
+            'metal_type': product_data.get('metal_type', ''),
+            'image_url': product_data.get('image_filename', '')
+        })
+    
     return products
 
-@app.route('/')
-def index():
-    products = get_webshop_db_connection() 
-    return render_template('index.html', products= products)
-
-# Bearbetningssida
 @app.route('/processImage', methods=['POST', 'GET'])
 def process_image():
     name = request.args.get('name')
@@ -62,43 +64,6 @@ def process_image():
     product_info = {'name': name, 'price': price, 'weight': weight, 'metal_type': metal_type}
     
     return render_template('processImage.html', product_info=product_info)
-
-@app.route('/saveProcessedData', methods=['POST'])
-def save_processed_data():
-    product_info = session.get('product_info')
-
-    if product_info:
-        try:
-            # Hämta data från session
-            product_data = product_info.split(',')
-            name = product_data[0]
-
-            # Spara den bearbetade bilden
-            processed_image = request.files['processed_image']
-            session_num = session.get("session_num", 0)
-            image_num = session.get("image_num", 1)
-            image_filename = os.path.join(app.config['ORDERS_FOLDER'], f'{session_num}_{image_num}.png')
-            processed_image.save(image_filename)
-            session["image_num"] = image_num + 1
-
-            # Spara information i databasen
-            conn = get_items_db_connection()  # Ersätt med din funktion för att få en databasanslutning
-            cursor = conn.cursor()
-            cursor.execute('INSERT INTO items (name, price, weight, metal_type, image_url) VALUES (?, ?, ?, ?, ?)',
-                           (name, product_data[1], product_data[2], product_data[3], os.path.basename(image_filename)))
-
-            conn.commit()
-            conn.close()
-
-            # Ta bort produktinfo från sessionen
-            session.pop('product_info', None)
-
-            return redirect('/index')
-        except Exception as e:
-            logging.error(str(e))
-            return jsonify({'error': 'Ett fel uppstod vid bearbetningen av datan.'}), 500
-    else:
-        return jsonify({'error': 'Produktinfo saknas i sessionen.'}), 400
 
 @app.route('/save', methods=['POST'])
 def save_image():
@@ -110,89 +75,101 @@ def save_image():
         if uploaded_file.filename == '':
             return jsonify({'error': 'Tomt filnamn. Ingen fil har laddats upp.'}), 400
 
-        product_info = json.loads(request.form.get('product_info'))  # Hämta produktinformationen som JSON
-        print("Produktinformation:", product_info)
-
-        # Hämta session-numret och bild-numret
+        product_info = json.loads(request.form.get('product_info'))
         session_num = session.get("session_num", 0)
-        print('Session-number:', session_num)
         image_num = session.get("image_num", 1)
 
-      # Generera filnamnet med session-nummer och bild-nummer
-        image_filename = os.path.join(app.config['ORDERS_FOLDER'], f'{session_num}_{image_num}.png')
-        uploaded_file.save(image_filename)
-        session["image_num"] = image_num + 1
-        print('image_filename', image_filename)
-        # Spara produktinformationen i databasen
-        conn = get_items_db_connection()  # Ersätt med din funktion för att få en databasanslutning
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO items (session_num, name, price, weight, metal_type, image_url) VALUES (?, ?, ?, ?, ?, ?)',
-               (session_num, product_info['name'], product_info['price'], product_info['weight'], product_info['metal_type'], os.path.basename(image_filename)))
+        image_filename = f'{session_num}_{image_num}.png'
+        uploaded_file.save(os.path.join(app.config['ORDERS_FOLDER'], image_filename))
+        session["image_num"] = int(image_num) + 1
 
-        conn.commit()
-        conn.close()
+        name = product_info['name']
+        price = product_info['price']
+        weight = product_info['weight']
+        metal_type = product_info['metal_type']
+       
+        save_data_to_realtime_db(name, price, weight, metal_type, image_filename,session_num )
+
         return jsonify({'message': 'Bilden har sparats på servern.'})
     except Exception as e:
         logging.error(str(e))
         return jsonify({'error': f'Ett fel uppstod vid sparandet av bilden och produktinformationen: {str(e)}'}), 500
-    
+
+def save_data_to_realtime_db(name, price, weight, metal_type, image_filename, session_num):
+    items_ref = db_ref.child('items')
+    new_item_ref = items_ref.push()
+    new_item_ref.set({
+        'name': name,
+        'price': price,
+        'weight': weight,
+        'metal_type': metal_type,
+        'image_url': image_filename,
+        'session_num': session_num
+    })
 @app.route('/cart')
 def cart():
     try:
         session_num = session.get('session_num', 0)
-
-        conn = get_items_db_connection()  # Ersätt med din funktion för att få en databasanslutning
-        cursor = conn.cursor()
-        cursor.execute('SELECT name, price, weight, metal_type, image_url, id FROM items WHERE session_num = ?', (session_num,))
-        ordered_items = cursor.fetchall()
-
+        
+        # Hämta produkter från Firebase-databasen
+        ordered_items = get_items_from_firebase(session_num)
         total_price = sum(item['price'] for item in ordered_items)  # Beräkna totalpriset
-
-        conn.close()
 
         return render_template('cart.html', ordered_items=ordered_items, total_price=total_price)
     except Exception as e:
         logging.error(str(e))
         return render_template('cart.html', ordered_items=[], total_price=0)
 
-@app.route('/remove_item/<item_id>', methods=['POST'])
+def get_items_from_firebase(session_num):
+    items_ref = db_ref.child('items')
+    #print(session_num)
+    #print(items_ref.order_by_child('session_num').get())
+  
+
+    ordered_items = []
+    query_result = items_ref.get()
+
+    for key, item in query_result.items():
+        print(type(item['session_num']), type((session_num)))
+        print(item)
+        ordered_items.append(item)
+            
+    return ordered_items
+
+
 @app.route('/remove_item/<int:item_id>', methods=['POST'])
 def remove_item(item_id):
     try:
-        # Hämta produktinformation från databasen
-        conn = get_items_db_connection()  # Anpassa detta enligt ditt system
-        cursor = conn.cursor()
-        cursor.execute('SELECT image_url FROM items WHERE id = ?', (item_id,))
-        item = cursor.fetchone()
-        
-        if item:
-            # Ta bort produkten från databasen
-            cursor.execute('DELETE FROM items WHERE id = ?', (item_id,))
-            conn.commit()
-            
-            # Ta bort bilden från orders-mappen
-            image_path = os.path.join('static', 'orders', item[0])
-            if os.path.exists(image_path):
-                os.remove(image_path)
-            
-            conn.close()
+        # Ta bort produkten från Firebase-databasen
+        if remove_item_from_firebase(item_id):
             return {'success': True}
         else:
             return {'success': False}
     except Exception as e:
         logging.error(str(e))
         return {'success': False}
-    
+
+def remove_item_from_firebase(item_id):
+    items_ref = db_ref.child('items')
+    item_ref = items_ref.child(item_id)
+    if item_ref.get():
+        item_ref.delete()
+        return True
+    return False
+
 # Visa orderhistorik
 @app.route('/orderHistory')
 def order_history():
-    conn = get_items_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM items')
-    order_history = cursor.fetchall()
-    conn.close()
-    
-    return render_template('orderHistory.html', order_history=order_history)
+    try:
+        order_history = get_all_items_from_firebase()
+        return render_template('orderHistory.html', order_history=order_history)
+    except Exception as e:
+        logging.error(str(e))
+        return render_template('orderHistory.html', order_history=[])
+
+def get_all_items_from_firebase():
+    items_ref = db_ref.child('items')
+    return items_ref.get()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/cart.js
+++ static/js/cart.js
@@ -1,9 +1,8 @@
-
-  // Ersätt med din Firebase-projektkonfiguration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Ersätt med din Firebase-projektkonfiguration
 const firebaseConfig = {
   apiKey: "AIzaSyDI_bZFy1g73Hq_SLZcgy3Y0w4SWPOmAu0",
   authDomain: "sns-jewllery.firebaseapp.com",
+  databaseURL: "https://sns-jewllery-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "sns-jewllery",
   storageBucket: "sns-jewllery.appspot.com",
   messagingSenderId: "390632077656",
@@ -11,42 +10,68 @@ const firebaseConfig = {
   measurementId: "G-VR33F4VQP4"
 };
 
-  // Initialisera Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialisera Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Hämta referens till Firestore-databasen
-  var db = firebase.firestore();
+// Hämta referens till Firestore-databasen
+var db = firebase.firestore();
 
-  // Hämta data från Firestore och uppdatera din HTML
-  db.collection("din_kollektion").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      var data = doc.data();
-      // Uppdatera din HTML med data här
-    });
+// Hämta data från Firestore och uppdatera din HTML
+db.collection("din_kollektion").get().then((querySnapshot) => {
+  var productContainer = document.querySelector(".product-list");
+
+  querySnapshot.forEach((doc) => {
+    var data = doc.data();
+    var productRow = document.createElement("div");
+    productRow.className = "product-row";
+
+    var productImage = document.createElement("img");
+    productImage.src = "/static/orders/" + data.image_url;
+    productImage.alt = data.name;
+    productImage.className = "product-image";
+    productRow.appendChild(productImage);
+
+    var productInfo = document.createElement("div");
+    productInfo.className = "product-info";
+    productInfo.innerHTML = `
+      <p><strong>Namn:</strong> ${data.name}</p>
+      <p><strong>Pris:</strong> ${data.price}</p>
+      <p><strong>Vikt:</strong> ${data.weight}</p>
+      <p><strong>Metalltyp:</strong> ${data.metal_type}</p>
+    `;
+    productRow.appendChild(productInfo);
+
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Ta bort";
+    removeButton.onclick = function() {
+      removeFromCart(data.name, doc.id);
+    };
+    productRow.appendChild(removeButton);
+
+    productContainer.appendChild(productRow);
   });
-
-
+});
 
 function removeFromCart(productName, itemId) {
-    fetch('/remove_item/' + itemId, {
-        method: 'POST',
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert(productName + " har tagits bort från kundvagnen.");
-            window.location.reload(); // Uppdatera sidan efter borttagningen
-        } else {
-            alert("Det uppstod ett fel. Försök igen senare.");
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert("Ett fel uppstod. Försök igen senare.");
-    });
+  fetch('/remove_item/' + itemId, {
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      alert(productName + " har tagits bort från kundvagnen.");
+      window.location.reload(); // Uppdatera sidan efter borttagningen
+    } else {
+      alert("Det uppstod ett fel. Försök igen senare.");
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("Ett fel uppstod. Försök igen senare.");
+  });
 }
 
 // Funktion för att gå tillbaka till startsidan när knappen klickas
 function goToHomePage() {
-    window.location.href = "/";
+  window.location.href = "/";
 }
